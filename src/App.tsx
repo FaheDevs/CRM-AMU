7f120707802aeb49844d4e54a3948847bb5025bf
fahed <zidanifahd@gmail.com>
2024-03-10 20:40:54 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className='flex h-screen'>
+      <div className='m-auto'>
+        <h1 className='text-3xl font-bold underline'>
+          Welcome to React Boilerplate
+        </h1>
+      </div>
+    </div>
+  )
+}
